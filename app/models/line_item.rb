597772e496e7cf45c9300e254e3bfc2706ffc520
Erfan Sharaf zadeh <erfan111@live.com>
2013-06-28 18:25:11 +0430

    fixed the previous commits, this one is complete i guess

---
+++ app/models/line_item.rb
@@ -0,0 +1,11 @@
+class LineItem < ActiveRecord::Base
+  attr_accessible :cart_id, :product_id ,:product ,:cart ,:quantity
+  belongs_to :cart
+  belongs_to :product
+  belongs_to :order
+  def total_price
+    product.price * quantity
+  end
+
+
+end

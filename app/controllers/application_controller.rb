24f1f4bab9b085029455c5d0d489e9f9e6f66bd5
alireza <sarsanaee@gmail.com>
2013-06-30 01:06:23 +0430

    adding just mobile view without cart actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :prepare_for_mobile
+
   private
+
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == '1'
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+   end
+  helper_method :mobile_device?
+
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+
+  end
 
   def current_user
     @current_user ||= User.find_by_auth_token(cookies[:auth_token]) if cookies[:auth_token]
